ff544f8a86ffc67636116f1e05f2a63336be3f0f
AlexSerodio <alequis01@gmail.com>
2018-12-16 00:35:36 -0200

    Update project

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className='main-footer'>
         <strong>
             Copyright &copy; 2018
-            <a href='https://github.com/furbprogIII20181/prog3-2018-2-controle_financeiro' target='_blank'>Fontes</a>
+            <a href='https://github.com/furbprogIII20181/prog3-2018-2-controle_financeiro' target='_blank' rel="noopener noreferrer">Fontes</a>
         </strong>
     </footer>
 )
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+import { Switch, Route, Redirect } from 'react-router'
 
-// import Dashboard from '../dashboard/dashboard'
-import Dashboard from '../dashboard2/dashboard2'
+import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
-import AuthOrApp from './authOrApp'
 
 export default props => (
-    <Router history={hashHistory}>
-        {/* primeiro parametro eh a url destino e o segundo eh o componente que sera exibido */}
-        <Route path='/' component={AuthOrApp}>
-            <IndexRoute component={Dashboard} />
-            <Route path='billingCycles' component={BillingCycle} />
-        </Route>
-        {/* se acessar qualquer url que nao as definidas acima, redireciona pra raiz */}
-        <Redirect from='*' to='/' />
-    </Router>
+    <div className='content-wrapper'>
+        <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/billingCycles' component={BillingCycle} />
+            <Redirect from='*' to='/' />
+        </Switch>
+    </div>
 )
